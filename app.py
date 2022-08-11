f7812005b6a024774d2b83b9ab35ad1174679aec
Danyal <danyalm2002@gmail.com>
2022-08-11 21:03:06 +0500

    added client secret and login code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, session, request
+from flask import Flask, render_template, url_for, redirect, session, request, abort
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 import datetime
@@ -10,9 +10,11 @@ import google.auth.transport.requests as requests
 import google.oauth2.id_token as id_token
 import pip._vendor.cachecontrol as cachecontrol
 import requests  # needed for google request lib above
+import pathlib
 
 KEY_SIZE = 6
 MAX_NOTE_LENGTH = 500
+DEBUG_MODE = True
 
 app = Flask(__name__)
 
@@ -27,6 +29,22 @@ db = SQLAlchemy(app)
 app.secret_key = os.urandom(KEY_SIZE)
 
 app.permanent_session_lifetime = datetime.timedelta(days=365)
+
+# allowing just for testing purposes
+# temp
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "313543264639-ti3ua7fftkkt6uucg7ih4nc81lm4jhc7.apps.googleusercontent.com"
+
+client_secrets_file = os.path.join(
+    pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile",
+            "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
 
 
 class Users(db.Model):
@@ -93,5 +111,61 @@ def session_clear():
     return "Success"
 
 
+@app.route('/login')
+def login():
+    authorisation_url, state = flow.authorization_url()
+    # ensuring the state is the same as google auth returns
+    session["state"] = state
+    return redirect(authorisation_url)  # redirect to auth screen
+
+
+# redirected here after login
+@app.route('/callback')
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(
+        session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token, request=token_request, audience=GOOGLE_CLIENT_ID)
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/user_notes")
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/')
+def index():
+    return "Home <br> <a href='/login'>Login</a>"
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)
+        else:
+            return function()
+    return wrapper
+
+
+@app.route('/user_notes')
+@login_is_required
+def user_notes():
+    return "Protected <br> <a href = '/logout'>Logout</a> "
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG_MODE)
