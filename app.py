7828783477e4d0fe7cc8e9b5e6d01da04d2cc4c6
Danyal <danyalm2002@gmail.com>
2022-08-15 21:24:50 +0500

    changed settings to host

--- app.py
+++ app.py
@@ -31,10 +31,6 @@ app.secret_key = os.urandom(KEY_SIZE)
 
 app.permanent_session_lifetime = datetime.timedelta(days=365)
 
-# allowing just for testing purposes
-# temp
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
 GOOGLE_CLIENT_ID = "313543264639-ti3ua7fftkkt6uucg7ih4nc81lm4jhc7.apps.googleusercontent.com"
 
 client_secrets_file = os.path.join(
@@ -44,7 +40,7 @@ flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile",
             "https://www.googleapis.com/auth/userinfo.email", "openid"],
-    redirect_uri="http://127.0.0.1:5000/callback"
+    redirect_uri="https://stickynotespromax.herokuapp.com/callback"
 )
 
 
